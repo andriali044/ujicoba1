217890e46ce3624575d8a0164207672d611efe3a
andriali044 <andriali044gmail.com>
2022-03-03 09:29:05 +0700

    test1

---
+++ taskreact17/src/App.js
@@ -0,0 +1,16 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+import { NewsContextProvider } from "./NewsContext";
+import News from "./components/News";
+import NewsArticle from "./components/NewsArticle";
+import "./app.css";
+
+function App() {
+  return (
+    <NewsContextProvider>
+      <News />
+    </NewsContextProvider>
+  );
+}
+
+export default App;
